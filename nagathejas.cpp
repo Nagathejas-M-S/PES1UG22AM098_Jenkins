15ca8bc95759b3b5ec9950043956d6c5524d736d
Nagathejas M S <thejasnaga@gmail.com>
2025-03-13 18:06:52 +0530

    adding nagathejas.cpp file

---
+++ nagathejas.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+
+  cout << "Now I have entered nagathejas.cpp file successfully\n";
+  cout << "By Nagathejas \n";
+  return 0;
+}

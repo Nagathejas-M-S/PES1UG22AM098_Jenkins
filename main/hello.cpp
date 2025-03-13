57f7ef9afbc80d8fb9a2051898048e1220bf9bdf
Nagathejas M S <thejasnaga@gmail.com>
2025-03-13 16:34:49 +0530

    hello.c modified successfully

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
+  cout << "Now I have committed the changes successfully\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
